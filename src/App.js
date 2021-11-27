4aa14543475ad732d7fe77ea9221976a3e1f33c2
Artem <ard.ryzhov@gmail.com>
2021-11-27 14:24:09 +0300

    create input and tag module

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+import './_reset.scss';
 import './App.scss';
 import Header from './components/Header';
 import About from './components/About';
 import FindInput from './components/FindInput/FindInput';
+import { useState } from 'react';
 
 function App() {
-  return (
-	<div className="content-wrapper">
-		<Header/>
-		<main id="main">
-			<About/>
-			<FindInput/>
-		</main>
-	</div>
-  );
+	const [tagInput, setTagInput] = useState([]);
+
+	return (
+		<div className="content-wrapper">
+			<Header/>
+			<main id="main">
+				<About/>
+				<FindInput
+				tagInput={tagInput}
+				setTagInput={setTagInput}/>
+			</main>
+		</div>
+	);
 }
 
 export default App;
--- src/components/FindInput/FindInput.jsx
+++ src/components/FindInput/FindInput.jsx
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Tag from '../Tag/Tag';
 
 import './_findInput.scss';
 
-const FindInput = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const FindInput = ({tagInput, setTagInput}) => {
+	const inputRef = useRef();
+
+	const tags = tagInput.map((tag, i) => {
+		return (
+			<Tag 
+			tag={tag}
+			setTagInput={setTagInput}
+			tagInput={tagInput}/>
+		)
+	})
+
+	function addToTagEnter(event) {
+		const tags = [...tagInput];
+		const currentTag = event.target.value;
+		console.log(tagInput)
+		if (event.code === 'Enter') {
+			setTagInput([
+				...tags,
+				{title: currentTag}
+			]);
+			event.target.value = '';
+		}
+	}
+
+	function addToTagIcon() {
+		const tags = [...tagInput];
+		const currentTag = inputRef.current.value;
+		console.log(tagInput)
+		setTagInput([
+			...tags,
+			{title: currentTag}
+		]);
+		inputRef.current.value = '';
+	}
+
 	return (
 		<div className="input-wrap">
-			
+			<div className="input-title">
+				<h3>Введите название продуктов которые у Вас есть.</h3>
+			</div>
+
+			<div className="input-field">
+				<input 
+				ref={inputRef}
+				type="text"
+				onKeyDown={(e) => addToTagEnter(e)}/>
+				<FontAwesomeIcon 
+				onClick={addToTagIcon}
+				icon={faSearch} 
+				className='input-search-icon'/>
+			</div>
+
+			<div className="input-tags-field">
+				<div className="input-tag">
+					<div className='input-tag-title'>Ключевые слова:</div>
+					<div className="input-tag-wrap">
+						{tags}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
